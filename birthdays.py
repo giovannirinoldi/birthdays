558ffeb296989169db5c56631f275b57290d5d7e
matti <900716@stud.unive.it>
2025-11-21 16:54:10 +0100

    Added proper documentation

--- birthdays.py
+++ birthdays.py
@@ -1,3 +1,4 @@
+# This module is used for listing and retrieving birthdays from the name of the person
 birthdays = {
     'Albert Einstein': '03/14/1879',
     'Benjamin Franklin': '01/17/1706',
@@ -6,11 +7,13 @@ birthdays = {
     'Rowan Atkinson': '01/6/1955'}
 
 def print_birthdays():
+    # Prints the whole list of names associated to their birthdays
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
     for name in birthdays:
         print(name)
 
 def return_birthday(name):
+    # Displays the birthday of a person given as input its name if the person's name is in the dictionary
     if name in birthdays:
         print('{}\'s birthday is {}.'.format(name, birthdays[name]))
     else:
